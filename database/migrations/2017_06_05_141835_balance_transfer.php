4a6a7bfe1092e5bb2c06297b282f28f459878f7c
Nur Karim <webinstitute772@gmail.com>
2017-06-05 21:20:08 +0600

    update database migration
    dd

--- database/migrations/2017_06_05_141835_balance_transfer.php
+++ database/migrations/2017_06_05_141835_balance_transfer.php
@@ -13,7 +13,17 @@ class BalanceTransfer extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('balance_transfers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('transfer_number');
+            $table->string('pin_number');
+            $table->string('transfer_user_id');
+            $table->string('amount');
+            $table->string('available_balance');
+            $table->string('status')->comment('1=complete 2=pending');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +33,6 @@ class BalanceTransfer extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('balance_transfers');
     }
 }
